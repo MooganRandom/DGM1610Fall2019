c77530f11de59b5b614e864d66f823a870d405c3
Megan D <megandenter331@gmail.com>
2019-08-26 17:11:09 -0600

    Added variables

---
+++ 2D Platform Game/Assets/Scripts/Assignment/Variables.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string name = "Megan";
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(name);
+    }
+
+    
+
+}    
