84b010fdea07d5ef728c28af1793e8e4bb600d9b
Megan D <megandenter331@gmail.com>
2019-08-28 17:04:25 -0600

    Added more variables

--- 2D Platform Game/Assets/Scripts/Assignment/Variables.cs
+++ 2D Platform Game/Assets/Scripts/Assignment/Variables.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    string name = "Megan";
+    string name = "Megan"; 
+    int age = 18;
+    float height = 5.5f;
+    bool married = false; 
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(name);
+        Debug.Log("First Name: " + name);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married: " + married);
     }
 
     
