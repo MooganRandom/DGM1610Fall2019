0d3ed5ec115c89a72a6bc97fc3c865f6429f9e0e
Megan Denter <megandenter331@gmail.com>
2019-09-25 15:58:01 -0600

    Fixed variables for Plane

--- Challenge (Plane)/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge (Plane)/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(30, 0, 10);
+    private float Vector3 = new Vector3(0f, 90f, 0f);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        FollowPlayerX.transform.position = new Vector3(0f, 90f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Offset Camera on right side of plane following
         transform.position = plane.transform.position + offset;
+
+        // Float rotate camera right of plane
+        transform.Rotate = (0, 90, 0);
     }
 }
