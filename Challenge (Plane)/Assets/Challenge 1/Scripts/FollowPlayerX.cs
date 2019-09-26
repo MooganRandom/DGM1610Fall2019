0f710744e46fb48c68c751a9ab28f5f9cbefbd62
Megan Denter <megandenter331@gmail.com>
2019-09-25 19:30:04 -0600

    Added function script and variables for Plane

--- Challenge (Plane)/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge (Plane)/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -6,12 +6,11 @@ public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
     private Vector3 offset = new Vector3(30, 0, 10);
-    private float Vector3 = new Vector3(0f, 90f, 0f);
 
     // Start is called before the first frame update
     void Start()
     {
-        FollowPlayerX.transform.position = new Vector3(0f, 90f, 0f);
+
     }
 
     // Update is called once per frame
@@ -19,8 +18,5 @@ public class FollowPlayerX : MonoBehaviour
     {
         // Offset Camera on right side of plane following
         transform.position = plane.transform.position + offset;
-
-        // Float rotate camera right of plane
-        transform.Rotate = (0, 90, 0);
     }
 }
